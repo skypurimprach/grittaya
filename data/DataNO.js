cb2602ecaeb75f9582bc784d8db4289f20efb2ae
purimprach27 <137262308+purimprach27@users.noreply.github.com>
2024-05-29 16:50:35 +0700

    .

--- data/DataNO.js
+++ data/DataNO.js
@@ -1,19 +1,23 @@
 const namob = [
-    { 
-      title: 'น้ำอบต้นตำหรับ', 
-      content: 'ฝาแดง', 
-      image: '/images/NO1.png'
-    },
-    {
-      title: 'น้ำอบสูตรชาววัง', 
-      content: 'ฝาเหลือง', 
-      image: '/images/NO2.png' 
-    },
-  ];
-  
-  export const getDataNO = () => {
-    return {
-        namob: namob
-    };
+  { 
+    title: 'น้ำอบต้นตำหรับ', 
+    content: 'ฝาแดง', 
+    image: '/images/NO1.png'
+  },
+  {
+    title: 'น้ำอบสูตรชาววัง', 
+    content: 'ฝาเหลือง', 
+    image: '/images/NO2.png' 
+  },
+  {
+    title: 'น้ำอบสูตรชาววัง', 
+    content: 'ฝาเหลือง', 
+    image: '/images/NO2.png' 
+  },
+];
+
+export const getDataNO = () => {
+  return {
+      namob: namob
   };
-  
+};
--- data/DataSKT.js
+++ data/DataSKT.js
@@ -1,19 +1,23 @@
 const skt = [
-    { 
-      title: 'สังฆทานแบบที่ 1', 
-      content: 'แบบที่1', 
-      image: '/images/SKT1.png'
-    },
-    {
-      title: 'สังฆทานแบบที่ 2', 
-      content: 'แบบที่ 2', 
-      image: '/images/SKT2.png'
-    },
-  ];
-  
-  export const getDataSKT = () => {
-    return {
-      skt: skt
-    };
+  { 
+    title: 'สังฆทานแบบที่ 1', 
+    content: 'แบบที่1', 
+    image: '/images/SKT1.png'
+  },
+  {
+    title: 'สังฆทานแบบที่ 2', 
+    content: 'แบบที่ 2', 
+    image: '/images/SKT2.png'
+  },
+  {
+    title: 'สังฆทานแบบที่ 3', 
+    content: 'แบบที่ 3', 
+    image: '/images/SKT2.png'
+  },
+];
+
+export const getDataSKT = () => {
+  return {
+    skt: skt
   };
-  
+};
