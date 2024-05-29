ea374d5a2746d79b02877fcf6e76b1ce7825280d
iicechyd <chayada.w@kkumail.com>
2024-05-29 11:32:10 +0700

    edit code

--- data/DataService.js
+++ data/DataService.js
@@ -3,25 +3,25 @@ const service = [
         title: 'ผู้ให้บริการจัดงานพิธี',
         description:
             'ผู้ให้บริการจัดงานพิธี งานบายศรี หรืองานบวงสรวงต่าง ๆ และให้คำปรึกษาในศาสตร์และศิลป์แขนงต่างๆ ที่สอนโดยผู้เชี่ยวชาญจากศาสตร์แต่ละแขนงครบวงจร ',
-        image: '/images/services/service1.jpg',
+        image: '/images/services/service1.png',
     },
     {
         title: 'รับทำบายศรีโบราณ และเครื่องบวงสรวง',
         description:
             'รับทำบายศรี โบราณ และเครื่อง บวงสรวง โดย อ.หนุ่ม อัญญาน้อย ไทเลย และคณะ',
-        image: '/images/services/service2.jpg',
+        image: '/images/services/service2.png',
     },
     {
         title: 'จำหน่ายผลิตภัณฑ์ เครื่องสักการะ',
         description:
             'จัดจำหน่ายผลิตภัณฑ์และเครื่องสักการะแบบโบราณไทย ในโอกาสต่างๆให้การทำบุญเป็นเรื่องง่ายด้วยชุดสังฆทาน จากกฤตธยา สวย ประณีต และใช้ประโยชน์ได้จริง',
-        image: '/images/services/service3.jpg',
+        image: '/images/services/service3.png',
     },
     {
         title: 'ดูดวง',
         description:
             'ดูดวงไพยิปซี ดูดวงตามราศี โหราศาสตร์ เรื่องการงาน การเงิน ความรัก สุขภาพ โชคลาภ บริวาร โดย อ.หนุ่ม อัญญาน้อย ไทเลย',
-        image: '/images/services/service4.jpg',
+        image: '/images/services/service4.png',
     },
 ]
 export const getDataService = () => {
--- pages/contactus.vue
+++ pages/contactus.vue
@@ -1,31 +1,43 @@
 <template>
-    <h1 class="p-10 text-center">
-        <nav class="mx-auto">
-            <a href="https://www.facebook.com/grittaya8?locale=th_TH">
-                <img
-                    class="img w-12 h-12 mx-auto"
-                    src="/images/icon facebook.webp"
-                />
+    <div
+        class="flex flex-col items-center justify-center min-h-screen p-10 space-y-10"
+    >
+        <div class="text-center">
+            <a
+                href="https://www.facebook.com/grittaya8?locale=th_TH"
+                class="flex flex-col items-center space-y-2"
+            >
+                <img class="img" src="/images/icons/fb.svg" />
+                <h1 class="text-base text-black">
+                    กฤตธยา รับจัดงานพิธี บายศรีโบราณ
+                </h1>
+                <h1 class="text-base text-black">
+                    เครื่องบวงสรวงและงานฝีมือเอกลักษณ์ไทย
+                </h1>
             </a>
-            <h1 class="h1 text-base text-black">
-                กฤตธยา รับจัดงานพิธี บายศรีโบราณ
-            </h1>
-            <h1 class="h1 text-base text-black">
-                เครื่องบวงสรวงและงานฝีมือเอกลักษณ์ไทย
-            </h1>
-            <br />
-            <a href="https://page.line.me/grittaya">
-                <img class="img w-8 h-8 mx-auto" src="/images/icon line.png" />
-                <h1 class="h1 text-base text-black">Grittaya</h1>
+        </div>
+        <div class="text-center">
+            <a
+                href="https://page.line.me/grittaya"
+                class="flex flex-col items-center space-y-2"
+            >
+                <img class="img" src="/images/icons/line.svg" />
+                <h1 class="text-base text-black">Grittaya</h1>
             </a>
-            <br />
-            <img class="img w-8 h-8 mx-auto" src="/images/icon tel.png" />
-            <h1 class="h1 text-base text-black">097-9905388</h1>
-            <h1 class="h1 text-base text-black">081-9958788</h1>
-        </nav>
-    </h1>
+        </div>
+        <div class="text-center flex flex-col items-center space-y-2">
+            <img class="img" src="/images/icons/tel.svg" />
+            <h1 class="text-base text-black">097-9905388</h1>
+            <h1 class="text-base text-black">081-9958788</h1>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.img {
+    width: 50px; /* Adjust the size as needed */
+    height: auto;
+}
+</style>
